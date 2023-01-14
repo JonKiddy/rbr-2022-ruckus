31cdfe806e637ef3ae7ab0595912d72ca211c846
Jonathan R. Kiddy <jon.kiddy@gmail.com>
2023-01-14 14:40:19 -0500

    added initiall commit for the swerve drive robot for ruckus

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,28 +21,28 @@ public final class Constants {
     // Find the Offset values. Follow the SDS configuration guidelines at:
     // https://github.com/SwerveDriveSpecialties/swerve-template
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 5;
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 6;
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 15;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 16;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 32;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(307.8);
-    // 32 encoder readings = 223.5, 178, 307.8
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(269.4);
+    // 32 encoder readings = 223.5, 178, 307.8, 142.8, 119.4, 142.7, 119.4, 119.4
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 12;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 11;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 34;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(301.3);
-    // 34 encoder readings = 37.8, 174, 301.3
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(126.6);
+    // 34 encoder readings = 37.8, 174, 301.3, 3.9, 126.6, 3.9, 126.6, 126.6
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 17;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 18;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 31;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(13.1);
-    // 31 encoder readings = 283.3, 187, 13.1
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(88.1);
+    // 31 encoder readings = 283.3, 187, 13.1, 251.5, 188.1, 251.5, 188.1, 188.1
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 4;
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 3;
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 14;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 13;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 33;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(49.2);
-    // 33 encoder readings = 322.3, 172, 49.2
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(5.5);
+    // 33 encoder readings = 322.3, 172, 49.2, 130.1, 230.5, 130.2, 230.5, 230.5
 
 }
